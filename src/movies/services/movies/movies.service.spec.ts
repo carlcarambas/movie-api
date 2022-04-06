99d2a8edb166fbf3deb6896ee64c0de28278d186
@vdr_carlmarkcarambas <carlmark.carambas@gmail.com>
2022-04-07 01:43:53 +0800

    fix service test

--- src/movies/services/movies/movies.service.spec.ts
+++ src/movies/services/movies/movies.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { MovieEntity } from '../../../models/movie.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { plainToClass } from 'class-transformer';
+
+const movies = [new MovieEntity(), new MovieEntity()];
+const createdMovie: MovieEntity = plainToClass(MovieEntity, {
+  id: 1,
+  title: 'A test todo',
+});
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  const mockedRepo = {
+    save: jest.fn((dto) => Promise.resolve(createdMovie)),
+    find: jest.fn((dto) => Promise.resolve(movies)),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MoviesService],
+      providers: [
+        MoviesService,
+        {
+          provide: getRepositoryToken(MovieEntity),
+          useValue: mockedRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
